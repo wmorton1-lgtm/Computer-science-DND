977d0320fd1564e17873102dc1459f5684b94f68
wmorton1-lgtm <wmorton1@hwemail.com>
2025-08-27 13:32:20 -0700

    Improved pugsaver
    Made pugsaver more time efficent

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -7,6 +7,17 @@ public class PugSaver {
 	public static void rescuePugs(ArrayList<Dog> list) {
 		for (int i = 0; i < list.size(); i++) {
 			if (list.get(i).getBreed().toLowerCase().contains("golden")) {
+				for (int j = list.size(); j < 0; j--) {
+					if (!list.get(j).getBreed().toLowerCase().contains("golden")) {
+						Dog fromBack = list.get(j);
+						Dog fromFront = list.get(i);
+						list.set(i, fromBack);
+						list.set(j, fromFront);
+					}
+				}
+
+				list.get(list.size() - 1);
+
 				list.add(list.get(i));
 				list.remove(i);
 			}
--- PugRescue/PugTester.java
+++ PugRescue/PugTester.java
@@ -5,13 +5,16 @@ public class PugTester {
     public static void main(String[] args) {
         ArrayList<Dog> dogs = new ArrayList<Dog>();
         Dog jef = new Dog("Jef", "Golden Retriever");
+        Dog xander = new Dog("Xander", "Golden Retriever");
         Dog tim = new Dog("Tim", "Pug Retriever");
         dogs.add(jef);
         dogs.add(tim);
+        dogs.add(xander);
         for (int i = 0; i < dogs.size(); i++) {
             System.out.println(dogs.get(i).toString());
             
         }
+        System.out.println("\n");
         PugSaver.rescuePugs(dogs);
         for (int i = 0; i < dogs.size(); i++) {
             System.out.println(dogs.get(i).toString());
