4ee1933331bcfb23c98157ef4548f7384fdfc073
wmorton1-lgtm <wmorton1@hwemail.com>
2025-10-03 15:25:47 -0700

    block 7 grind doubly linked list

--- DoublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -36,7 +36,6 @@ public class DoublyLinkedList {
 		return SENTINEL.getPrevious();
 	}
 
-
 	// Returns true if this list is empty; otherwise returns false.
 	public boolean isEmpty() {
 		return nodeCount == 0;
@@ -57,22 +56,11 @@ public class DoublyLinkedList {
 	// if not found, returns -1.
 	public int indexOf(Nucleotide obj) {
 		int index = 0;
-		if (obj == null) {
-			for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.equals(SENTINEL); n =
-					n.getNext()) {
-				if (n.getValue() == null) {
-					return index;
-				}
-				index++;
+		for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.getNext().equals(SENTINEL); n = n.getNext()) {
+			if (n.getValue() == obj) {
+				return index;
 			}
-		} else {
-			for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.equals(SENTINEL); n =
-					n.getNext()) {
-				if (n.getValue().equals(obj)) {
-					return index;
-				}
-				index++;
-			}
+			index++;
 		}
 		return -1;
 	}
@@ -81,12 +69,18 @@ public class DoublyLinkedList {
 	// otherwise returns false.
 	public boolean add(Nucleotide obj) {
 		ListNode2<Nucleotide> toAdd = new ListNode2<Nucleotide>(obj);
-
-		toAdd.setPrevious(SENTINEL.getPrevious());
-		SENTINEL.getPrevious().setNext(toAdd);
-		toAdd.setNext(SENTINEL);
-		SENTINEL.setPrevious(toAdd);
-		nodeCount++;
+		if (obj == null) {
+			toAdd.setPrevious(SENTINEL.getPrevious());
+			SENTINEL.getPrevious().setNext(toAdd);
+			toAdd.setNext(SENTINEL);
+			SENTINEL.setPrevious(toAdd);
+		} else {
+			toAdd.setPrevious(SENTINEL.getPrevious());
+			SENTINEL.getPrevious().setNext(toAdd);
+			toAdd.setNext(SENTINEL);
+			SENTINEL.setPrevious(toAdd);
+			nodeCount++;
+		}
 		return true;
 	}
 
@@ -107,40 +101,91 @@ public class DoublyLinkedList {
 			}
 			index++;
 		}
-		return false;2
+		return false;
 	}
 
 	// // Returns the i-th element.
-	// public Nucleotide get(int i) {}
+	public Nucleotide get(int i) {
+		if (i >= nodeCount || i < 0) {
+			throw new IndexOutOfBoundsException("get() -  the inex was out of bounds");
+		}
+		int index = 0;
+		for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.equals(SENTINEL); n = n.getNext()) {
+			if (index == i) {
+				return n.getValue();
+			}
+			index++;
+		}
+		throw new IndexOutOfBoundsException("get() -  the index was out of bounds & got thru check");
+	}
 
 	// // Replaces the i-th element with obj and returns the old value.
-	// public Nucleotide set(int i, Nucleotide obj) {}
+	public Nucleotide set(int i, Nucleotide obj) {
+		if (i >= nodeCount || i < 0) {
+			throw new IndexOutOfBoundsException("set(index, obj) -  the inex was out of bounds");
+		}
+		if (obj == null) {
+			throw new NullPointerException("set(index, obj) - nucleotide was null");
+		}
+		int index = 0;
+		for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.equals(SENTINEL); n = n.getNext()) {
+			if (index == i) {
+				Nucleotide toreturn = n.getValue();
+				n.setValue(obj);
+				return toreturn;
+			}
+			index++;
+		}
+		throw new IndexOutOfBoundsException("set(index, obj) -  the index was out of bounds & got thru check");
+	}
 
 	// // Inserts obj to become the i-th element. Increments the size
 	// // of the list by one.
-	// public void add(int i, Nucleotide obj) {}
+	public void add(int i, Nucleotide obj) {
+		if (i > nodeCount || i < 0) {
+			throw new IndexOutOfBoundsException("add(i, obj) -  the inex was out of bounds");
+		}
+		int index = 0;
+		ListNode2<Nucleotide> objNode = new ListNode2<Nucleotide>(obj);
+
+		for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.equals(SENTINEL); n = n.getNext()) {
+			if (i == index) {
+				objNode.setPrevious(n.getPrevious());
+				n.getPrevious().setNext(objNode);
+				objNode.setNext(n);
+				n.setPrevious(objNode);
+				nodeCount++;
+			}
+			index++;
+		}
+	}
 
 	// // Removes the i-th element and returns its value.
 	// // Decrements the size of the list by one.
-	// public Nucleotide remove(int i) {
-	// 	int index = 0;
-	// 	int objIndex = indexOf(obj);
-	// 	if (objIndex == -1) {
-	// 		return false;
-	// 	}
-	// 	for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.equals(SENTINEL); n = n.getNext()) {
-	// 		if (index == objIndex) {
-	// 			toRemove.getPrevious().setNext(n);
-	// 		}
-	// 		index++;
-	// 	}
-	// }
+	// instance variables FIX FIX FIX FIX FIX FIX FIX FIX FIX FIX 
+	public Nucleotide remove(int i) {
+		if (i >= nodeCount || i < 0) {
+			throw new IndexOutOfBoundsException("remove(i) -  the inex was out of bounds");
+		}
+		int index = 0;
+		for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.equals(SENTINEL); n = n.getNext()) {
+			if (i == index) {
+				Nucleotide toReturn = n.getValue();
+				n.getPrevious().setNext(n.getNext());
+				n.getNext().setPrevious(n.getPrevious());
+				return toReturn;
+			}
+			index++;
+		}
+		throw new IndexOutOfBoundsException("remove(i) - index passed check & nothing was removed");
+	}
 
-	// Returns a string representation of this list exactly like that for MyArrayList.
+	// Returns a string representation of this list exactly like that for
+	// MyArrayList.
 	public String toString() {
 		StringBuilder sting = new StringBuilder("[");
 
-		for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.equals(SENTINEL); n = n.getNext()) {
+		for (ListNode2<Nucleotide> n = SENTINEL.getNext(); !n.getNext().equals(SENTINEL); n = n.getNext()) {
 			sting.append(n.getValue());
 			sting.append(", ");
 		}
@@ -156,8 +201,10 @@ public class DoublyLinkedList {
 	// }
 
 	// // Like question 8 on the SinglyLinkedList test:
-	// // You are to remove the next 16 nodes from the list, after the node nodeBefore.
-	// // (on the test these nodes were assumed to contain CCCCCCCCGGGGGGGG, but here
+	// // You are to remove the next 16 nodes from the list, after the node
+	// nodeBefore.
+	// // (on the test these nodes were assumed to contain CCCCCCCCGGGGGGGG, but
+	// here
 	// // you do not need to assume or check for that)
 	// public void removeCCCCCCCCGGGGGGGG(ListNode2<Nucleotide> nodeBefore) {
 
--- DoublyLinkedList/XanderIsStupid.java
+++ DoublyLinkedList/XanderIsStupid.java
@@ -1,21 +1,37 @@
 public class XanderIsStupid {
     public static void main(String[] args) {
-        Nucleotide[] tides = new Nucleotide[20];
+        Nucleotide[] tides = new Nucleotide[10];
         tides[0] = Nucleotide.G;
-        DoublyLinkedList foo = new DoublyLinkedList(tides);
-        // System.out.println(foo.toString());
         tides[1] = Nucleotide.A;
-        // System.out.println(foo1.toString());
         tides[2] = Nucleotide.T;
         tides[3] = Nucleotide.T;
         tides[4] = Nucleotide.C;
         tides[5] = Nucleotide.A;
         tides[6] = Nucleotide.T;
-        Nucleotide evan = Nucleotide.T;
+        tides[7] = Nucleotide.T;
+        DoublyLinkedList xandersNucleotides = new DoublyLinkedList(tides);
+        System.out.println(xandersNucleotides.toString());
+        Nucleotide evan = Nucleotide.A;
+        // instance variables
+        // constructors
+        // getters
+        // setters
+        // methods
 
-        DoublyLinkedList foo2 = new DoublyLinkedList(tides);
-        System.out.println(foo2.toString());
-        System.out.println(foo.indexOf(Nucleotide.T));
+        // System.out.println(xandersNucleotides.indexOf(Nucleotide.T));
+        // System.out.println(xandersNucleotides.indexOf(null));
+        // System.out.println(xandersNucleotides.get(0));
+        // System.out.println(xandersNucleotides.get(5));
+        // xandersNucleotides.add(8, evan);
+        // xandersNucleotides.set(0, evan);
+        // xandersNucleotides.set(7, evan);
+        // System.out.println(xandersNucleotides.remove(7));
+        System.out.println(xandersNucleotides.toString());
+
+
+
+
+
         
     }
 }
