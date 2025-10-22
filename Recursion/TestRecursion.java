ee265dcc849dde66632574e3d6bd8ca2aa3df411
wmorton1-lgtm <wmorton1@hwemail.com>
2025-10-22 15:30:28 -0700

    Update TestRecursion.java

--- Recursion/TestRecursion.java
+++ Recursion/TestRecursion.java
@@ -1,18 +1,18 @@
 public class TestRecursion {
     public static void main(String[] args) {
-        // printlistinreverse
-        ListNode one = new ListNode("One");
-        ListNode two = new ListNode("Two");
-        ListNode three = new ListNode("Three");
-        one.setNext(two);
-        two.setNext(three);
-        Recursion.printListInReverse(one);
+        // // printlistinreverse
+        // ListNode one = new ListNode("One");
+        // ListNode two = new ListNode("Two");
+        // ListNode three = new ListNode("Three");
+        // one.setNext(two);
+        // two.setNext(three);
+        // Recursion.printListInReverse(one);
 
         // infect
         String[][] list = new String[5][5];
-        for (int i = 0; i < args.length; i++) {
-            for (int j = 0; j < args.length; j++) {
-                list[i][j] = j + "";
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                list[i][j] = (j+1) + "";
             }
         }
         Recursion.print2DArray(list);
