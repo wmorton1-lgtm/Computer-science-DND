14cc6ac2011b0f2d34367ec64f9f557f09224f58
wmorton1-lgtm <wmorton1@hwemail.com>
2026-01-26 15:04:15 -0800

    work from class 1/23 and afterwards

--- Stack/Arithmetic.java
+++ Stack/Arithmetic.java
@@ -9,82 +9,130 @@ public class Arithmetic {
     // Returns the result of doing operand1 operation operand2,
     // e.g. operate(5, 2, "-") should return 3
     public static int operate(int operand1, int operand2, String operation) {
-        
+        if (operation == null || operation.equals("")) {
+
+        }
+        if (operation == "-") {
+            return operand1 - operand2;
+        }
+        if (operation == "+") {
+            return operand1 + operand2;
+        }
+        if (operation == "*") {
+            return operand1 * operand2;
+        }
+        if (operation == "%") {
+            return operand1 % operand2;
+        }
+        if (operation == "/") {
+            return operand1 / operand2;
+        }
+        if (operation == "^") {
+            return (int) Math.pow(operand1, operand2);
+        }
+        throw new IllegalArgumentException("operate(): invalid operator");
     }
 
     // Evaluates a String exp that has an arithmetic expression written in STOUT
     // notation
     public static int evaluateStout(String exp) {
-        return 0;
+        char[] array = exp.toCharArray();
+        for (int i = 0; i < array.length - 2; i++) {
+            if (i < 2 && !isOperator(array[i])) {
+                
+            } else if (isOperator(array[i])) {
+                operate(Character.valueOf(array[i - 2]), Character.valueOf(array[i - 1]), array[i] + "");
+            } else {
+                throw new IllegalArgumentException("evaluateStout(): invalid stout format");
+            }
+        }
     }
 
     public static String convertClassicToStout(String exp) {
-        MyStack<Character> operatorStack = new MyStack<Character>();
+        MyStack<Character> oppStack = new MyStack<Character>();
         char[] expArray = exp.toCharArray();
-        String operatorListString = "()-+/*^%";
-        char[] operatorList = operatorListString.toCharArray();
         StringBuilder stoutForm = new StringBuilder();
-        
+
 
         for (int i = 0; i < expArray.length; i++) {
             char charI = expArray[i];
 
-            if ((int) charI >= 48 && (int) charI <= 57) { // if character number
+            if (charI == ' ') {
+                
+            } else if (charI == '(') {
+                oppStack.push(charI);
 
-            } else if (charI == '(' || charI == ')' || (charI == '-' && expArray[i + 1] == ' ')
-                    || charI == '+' || charI == '/' || charI == '*' || charI == '^'
-                    || charI == '%') {// if its an operation
+            } else if (charI == ')') {
+                while (!oppStack.empty() && oppStack.peek() != '(') {
+                    stoutForm.append(oppStack.pop());
+                }
+                if (oppStack.empty()) {
+                    throw new IllegalArgumentException(
+                            "convertClassicToStout(): Invalid character");
+                }
+                oppStack.pop();
 
-                    stoutForm.append(charI);
-                if (operatorStack.peek().compareTo(charI) <= 0) {
-                    for (char current = operatorStack.peek(); !operatorStack.empty(); operatorStack.pop()) {
-                    
+            } else if ((int) charI >= 48 && (int) charI <= 57) {
+                stoutForm.append(charI);
+
+            } else {
+                if (isOperator(charI)) {
+                    if (charI == '-' && ((int) expArray[i+1] >= 48 && (int) expArray[i+1] <= 57)) {
+                        
                     }
-                    operatorStack.push(charI);
+                    if (oppStack.empty() || isOperatorAGreaterThanB(charI, oppStack.peek())) {
+                        oppStack.push(charI);
+                    } else {
+                        while (!oppStack.empty() && oppStack.peek() != '('
+                                && isOperatorAGreaterThanB(oppStack.peek(), charI)) {
+                            stoutForm.append(oppStack.pop());
+                        }
+                        oppStack.push(charI);
+                    }
+                } else {
+                    throw new IllegalArgumentException(
+                            "convertClassicToStout(): Invalid character");
                 }
             }
         }
-        return "";
+
+        while (!oppStack.empty()) {
+            if (oppStack.pop() == '(') {
+                throw new IllegalArgumentException("convertClassicToStout(): Invalid character");
+            }
+            stoutForm.append(oppStack.pop());
+        }
+        return stoutForm + "";
     }
 
-    public boolean isOperatorAGreaterThanB(char a, char b) {
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^';
+    }
+
+    public static boolean isOperatorAGreaterThanB(char a, char b) {
         int AValue = -1;
         int BValue = -1;
-        if (a == ')') { 
-            BValue = 0;
-        }
-        if (b == ')') { 
-            BValue = 0;
-        }
 
         if (a == '+' || a == '-') {
             AValue = 1;
         }
-        if (b == '+' || b == '-') {
-            BValue = 1;
-        }
-
         if (a == '*' || a == '/' || a == '%') {
             AValue = 2;
+        }
+        if (a == '^') {
+            AValue = 3;
+        }
+
+        if (b == '+' || b == '-') {
+            BValue = 1;
         }
         if (b == '*' || b == '/' || b == '%') {
             BValue = 2;
         }
-
-        if (a == '^') {
-            AValue = 3;
-        }
         if (b == '^') {
-            AValue = 3;
+            BValue = 3;
         }
 
-        if (a == '(') { 
-            BValue = 4;
-        }
-        if (b == '(') { // TO-DO:
-            BValue = 4;
-        }
-
-        return AValue > BValue;
-    } 
+        return (AValue > BValue) || (AValue == BValue && b != '^');
+    }
 }
