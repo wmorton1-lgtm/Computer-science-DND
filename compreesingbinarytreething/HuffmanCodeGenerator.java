426ee140a640c517d97d0e8dde4a3b364cc4428a
wmorton1-lgtm <wmorton1@hwemail.com>
2026-04-21 09:25:09 -0700

    work from class on 4/21

--- compreesingbinarytreething/HuffmanCodeGenerator.java
+++ compreesingbinarytreething/HuffmanCodeGenerator.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.IOException;
+import java.util.PriorityQueue;
 
 public class HuffmanCodeGenerator {
     private HashMap<Character, Integer> characterFrequencyGrid;
     private char EOF = 26;
+    private ArrayList<Character> charListInOrderOfFrequency;
+    private PriorityQueue<Character> charHeap;
 
     public HuffmanCodeGenerator(String frequencyFile) throws IOException {
         characterFrequencyGrid = new HashMap<Character, Integer>();
@@ -24,6 +26,7 @@ public class HuffmanCodeGenerator {
                 int charFrequency = getFrequency(c);
                 characterFrequencyGrid.put(c, charFrequency + 1);
             } else {
+                charHeap.add(c);
                 characterFrequencyGrid.put(c, 1);
             }
             charAsInt = br.read();
@@ -39,6 +42,12 @@ public class HuffmanCodeGenerator {
             return frequency;
         } else {
             return 0;
+        }
+    }
+
+    public void sortCharArray() {
+        for (int i = 0; i < charHeap.size(); i++) {
+            charListInOrderOfFrequency.add(charHeap.poll());
         }
     }
 }
